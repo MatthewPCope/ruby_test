625605b16e839f6d26e14f6e28d94b5bf795281f
matthewpcope <matthew.p.cope@gmail.com>
2023-12-14 11:26:19 -0800

    added a few things

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "about", to: "about#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root to: "main#index"
+  
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
+
+# Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+# Defines the root path route ("/")
+# root "posts#index"
+# Can be used by load balancers and uptime monitors to verify that the app is live.
